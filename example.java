1c7862961fc3a784314b80f343f1023e2ea8f862
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-10 15:47:56 +0200

    Update example.java

--- example.java
+++ example.java
@@ -1,8 +1,6 @@
 /**
 * Returns an Image object that can then be painted on the screen. 
 * The url argument must specify an absolute <a href="#{@link}">{@link URL}</a>. The name
-* argument is a specifier that is relative to the url argument. 
-* <p>
 * This method always returns immediately, whether or not the 
 * image exists. When this applet attempts to draw the image on
 * the screen, the data will be loaded. The graphics primitives 
