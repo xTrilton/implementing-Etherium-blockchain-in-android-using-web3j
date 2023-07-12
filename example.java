7c53da4eb100de464a18dbaa7ca809fac2ad2dff
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-12 12:41:42 +0200

    Update example.java

--- example.java
+++ example.java
@@ -1,18 +1,8 @@
-/**
-* This method always returns immediately, whether or not the 
-* image exists. When this applet attempts to draw the image on
-* the screen, the data will be loaded. The graphics primitives 
-* that draw the image will incrementally paint on the screen. 
-* @param  url  an absolute URL giving the base location of the image
-* @param  name the location of the image, relative to the url argument
-* @return  the image at the specified URL
-* @see Image
-*/
+public class Main {
+  int x = 5;
 
-public Image getImage(URL url, String name) {
-try {
-return getImage(new URL(url, name));
-} catch (MalformedURLException e) {
-return null;
-}
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
 }
