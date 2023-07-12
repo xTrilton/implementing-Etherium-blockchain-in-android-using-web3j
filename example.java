d6a22e0185797fbacade2241e1d68dc1e558bc27
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-12 10:22:54 +0200

    Update example.java

--- example.java
+++ example.java
@@ -6,6 +6,7 @@
 * @param  url  an absolute URL giving the base location of the image
 * @param  name the location of the image, relative to the url argument
 * @return  the image at the specified URL
+sdfsdfff
 * @see Image
 */
 
