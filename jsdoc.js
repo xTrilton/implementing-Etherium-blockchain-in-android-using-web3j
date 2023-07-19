2b0a65ffa5b6423f18686164f022cbfeee509657
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-19 20:03:03 +0200

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -6,3 +6,12 @@ console.log(myFunctionVar());    // => 42
 console.log(myFunctionVar.name); // => 'getNumber'
 
 console.log(typeof getNumber);   // => 'undefined'
+/**
+ * @param {string}  p1 - A string param.
+ * @param {string=} p2 - An optional param (Google Closure syntax)
+ * @param {string} [p3] - Another optional param (JSDoc syntax).
+ * @param {string} [p4="test"] - An optional param with a default value
+ * @returns {string} This is the result
+ */
+
+
