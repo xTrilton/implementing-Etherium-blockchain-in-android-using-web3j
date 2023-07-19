c0f160af2f2071ba029bd0fb7d30fdd6944ec566
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-19 19:24:46 +0200

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -1,3 +1,13 @@
+
+// Parameters may be declared in a variety of syntactic forms
+/**
+ * @param {string}  p1 - A string param.
+ * @param {string=} p2 - An optional param (Google Closure syntax)
+ * @param {string} [p3] - Another optional param (JSDoc syntax).
+ * @param {string} [p4="test"] - An optional param with a default value
+ * @returns {string} This is the result
+ */
+
 const myFunctionVar = function getNumber() {
   console.log(typeof funName === 'function'); // => true
   return 42;
