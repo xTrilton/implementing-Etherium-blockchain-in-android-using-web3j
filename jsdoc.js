2f226d2444da69515ca9776817a050e4ae66249d
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-19 21:04:05 +0200

    Update and rename jsdoc.js to doc.js

--- jsdoc.js
+++
@@ -1,8 +0,0 @@
-const myFunctionVar = function getNumber() {
-  console.log(typeof funName === 'function'); // => true
-  return 42;
-}
-console.log(myFunctionVar());    // => 42
-console.log(myFunctionVar.name); // => 'getNumber'
-
-console.log(typeof getNumber);   // => 'undefined'
