6067d2f327c6b00f37d28144d7454f36090d4266
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-19 20:57:17 +0200

    Update wame.js

--- wame.js
+++ wame.js
@@ -7,3 +7,12 @@ function ps() {}
  * @returns {{ a: string, b: number }} - May use '@returns' as well as '@return'
  */
 function ab() {}
+
+/**
+ * @typedef {Object} SpecialType - creates a new type named 'SpecialType'
+ * @property {string} prop1 - a string property of SpecialType
+ * @property {number} prop2 - a number property of SpecialType
+ * @property {number=} prop3 - an optional number property of SpecialType
+ * @prop {number} [prop4] - an optional number property of SpecialType
+ * @prop {number} [prop5=42] - an optional number property of SpecialType with default
+ */
