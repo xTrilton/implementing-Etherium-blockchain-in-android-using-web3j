6752154a48e93b99944baaf6306037f294fe3b3d
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-19 21:00:17 +0200

    Update wame.js

--- wame.js
+++ wame.js
@@ -7,3 +7,7 @@ function ps() {}
  * @returns {{ a: string, b: number }} - May use '@returns' as well as '@return'
  */
 function ab() {}
+
+function special(options) {
+  return (options.prop4 || 1001) + options.prop5;
+}
