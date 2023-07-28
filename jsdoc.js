a14871de976b869046f477ae5b748822c2820cae
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-07-28 13:44:10 +0200

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -2,7 +2,7 @@ const myFunctionVar = function getNumber() {
   console.log(typeof funName === 'function'); // => true
   return 42;
 }
-console.log(myFunctionVar());    // => 42
+console.log(myFunctionVar());    // => 42  ;l,
 console.log(myFunctionVar.name); // => 'getNumber'
 
 console.log(typeof getNumber);   // => 'undefined'
