c2a1d1f089ac557393ced8428a73d4e5168825aa
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-08-11 16:48:18 +0200

    Create bojosi.js

---
+++ src/bojosi.js
@@ -0,0 +1,2 @@
+Request changes and post a comment that explains the copyright header requirement.
+All new files in the /src directory must include the required copyright header at the top of the file:
