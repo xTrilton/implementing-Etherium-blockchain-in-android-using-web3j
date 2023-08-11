d6414ba0c4e5745a816da6799fd6ab3c2f8c0c4c
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-08-11 17:13:28 +0200

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -7,3 +7,6 @@ console.log(myFunctionVar());    // => 42
 console.log(myFunctionVar.name); // => 'getNumber'
 
 console.log(typeof getNumber);   // => 'undefined'
+
+// Copyright (c) ORG  and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details
