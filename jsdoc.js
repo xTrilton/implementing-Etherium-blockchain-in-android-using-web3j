5031020bc88c84003fb8b3bca80d9eb505df4d09
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-08-12 11:11:37 +0200

    Update jsdoc.js

--- jsdoc.js
+++ jsdoc.js
@@ -1,12 +1,1 @@
-const myFunctionVar = function getNumber() {
-  console.log(typeof funName === 'function'); // => true
-  return 42;
-  dfsfd
-}
-console.log(myFunctionVar());    // => 42
-console.log(myFunctionVar.name); // => 'getNumber'
-
-console.log(typeof getNumber);   // => 'undefined'
-
-// Copyright (c) ORG  and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for details
+fdf
