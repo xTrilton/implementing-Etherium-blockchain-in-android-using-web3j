43dd6dddbb5948e7bcea955fbc960e74de044f30
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-09-14 10:16:43 +0200

    Update wame.js

--- src/app/wame.js
+++ src/app/wame.js
@@ -1,7 +1,10 @@
-/**
- * @return {PromiseLike<string>}
- */
+
 function ps() {}
+ 
+eturns {{ a: string, b: number }} - May use '@returns' as well as '@return'
+ */
+function ab() {}
+
  
 /**
  * @returns {{ a: string, b: number }} - May use '@returns' as well as '@return'
