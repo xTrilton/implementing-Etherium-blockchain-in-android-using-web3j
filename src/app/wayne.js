704c853a915cf9cdc002de2687d077219ad2f3bc
Boemo W Mmopelwa <boemommopelwa@gmail.com>
2023-09-14 20:15:35 +0200

    Create wayne.js

---
+++ src/app/wayne.js
@@ -0,0 +1,11 @@
+/**
+ * @returns {{ a: string, b: number }} - May use '@returns' as well as '@return'
+ */g, b: number }} - May use '@returns' as well as '@return'
+ */
+function ab() {}/**
+ * @returns {{ a: string, b: number }} - May use '@returns' as well as '@return'
+ */
+function ab() {}/**
+ * @returns {{ a: string, b: number }} - May use '@returns' as well as '@return'
+ */
+function ab() {}
